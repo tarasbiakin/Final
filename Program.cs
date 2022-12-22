a8a0760131016e2116402b63573e5c9d2a1ee37d
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-12-22 13:16:39 +0300

    метод выборки нужных элементов массива

--- Program.cs
+++ Program.cs
@@ -2,4 +2,23 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 //лучше обойтись исключительно массивами
 
- 
+ string[] arrString = { "хороший", "плохой", "злой", "jq", "jq", "j1", "ji", "112", "kj1" };
+//string[] arrString = Console.ReadLine().Split(' ').Select(x => string.Parse(x)).ToArray();
+
+string[] ArrayStringLenth(string[] arrString)
+{
+    int count = 0;
+    string[] arrStringNew = new string[arrString.Length];
+    for (int i = 0; i < arrString.Length; i++)
+    {
+        if (arrString[i].Length <= 3)
+        {
+            arrStringNew[count] = arrString[i];
+            count++;
+            // Console.WriteLine(count);
+        }
+
+    }
+    return arrStringNew;
+
+}
